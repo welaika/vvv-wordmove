a88ae0ccda067b420aae0652341ce18d9c30dd4b
Daniele Scasciafratte <mte90net@gmail.com>
2020-09-22 14:08:08 +0200

    code optimization

--- bin/provision.sh
+++ bin/provision.sh
@@ -5,29 +5,20 @@
 if [ $(command -v gem) ]; then
 	echo "gem installed"
 else
-        echo "ruby-dev & gem package installing..."
-	apt remove ruby* -y
-	snap install ruby --classic
-	gem install rubygems-update
+    echo "ruby-dev & gem package installing..."
+    apt remove ruby* -y
+    snap install ruby --classic
+    gem install rubygems-update
 fi
 
 # wordmove install
-wordmove_install="$(gem list wordmove -i)"
-if [ "$wordmove_install" = true ]; then
-  echo "wordmove installed"
+gem update wordmove
+
+wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
+if [  "$(grep yaml $wordmove_path)" ]; then
+    echo "can require yaml"
 else
-    echo "wordmove not installed"
-    gem install wordmove
-
-    wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
-    if [  "$(grep yaml $wordmove_path)" ]; then
-        echo "can require yaml"
-    else
-        echo "can't require yaml"
-        echo "set require yaml"
-
-        sed -i "7i require\ \'yaml\'" $wordmove_path
-
-        echo "can require yaml"
-    fi
+    echo "set require yaml"
+    sed -i "7i require\ \'yaml\'" $wordmove_path
+    echo "can require yaml"
 fi
